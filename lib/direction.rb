c12043c0a264ca87dc073c7ee03e37973ac59596
Jim Gay <jim@saturnflyer.com>
2014-10-03 15:13:43 -0400

    cleanup the weirdo code it had before by using a block in a utility method

--- lib/direction.rb
+++ lib/direction.rb
@@ -16,40 +16,36 @@ require "direction/version"
 # relationship between objects.
 module Direction
 
-  # Generates a string sufficient to forward messages to the accessor.
-  # Specify the return_value for the result of the forwarded message.
-  def self.message_handler(message, accessor, return_value)
-    %{
-    def #{message}(*args)
-      if block_given?
-        #{accessor}.__send__(:#{message}, *args) { |*block_args| yield(*block_args) }
-      else
-        #{accessor}.__send__(:#{message}, *args)
-      end
-      #{return_value}
-    end
-    }
-  end
-
-  # Loops over a hash of options and defines methods to the apply_to module/class provided.
-  # Specify the return_value for the result of all forwarded messages.
-  def self.setup_forwarding(options, apply_to:, return_value: '')
-    method_defs = []
-    options.each_pair do |key, value|
-      Array(key).map do |command_name|
-        method_defs.unshift Direction.message_handler(command_name, value, return_value)
-      end
-    end
-    apply_to.class_eval method_defs.join(' '), __FILE__, __LINE__
-  end
-
   # Forward messages and return self, protecting the encapsulation of the object
   def command(options)
-    Direction.setup_forwarding(options, apply_to: self, return_value: 'self')
+    Direction.define_methods(self, options) do |message, accessor|
+      %{
+      def #{message}(*args, &block)
+        #{accessor}.__send__(:#{message}, *args, &block)
+        self
+      end
+      }
+    end
   end
 
   # Forward messages and return the result of the forwarded message
   def query(options)
-    Direction.setup_forwarding(options, apply_to: self)
+    Direction.define_methods(self, options) do |message, accessor|
+      %{
+      def #{message}(*args, &block)
+        #{accessor}.__send__(:#{message}, *args, &block)
+      end
+      }
+    end
+  end
+
+  def self.define_methods(mod, options)
+    method_defs = []
+    options.each_pair do |method_names, accessor|
+      Array(method_names).map do |message|
+        method_defs.unshift yield(message, accessor)
+      end
+    end
+    mod.class_eval method_defs.join("\n"), __FILE__, __LINE__
   end
 end
